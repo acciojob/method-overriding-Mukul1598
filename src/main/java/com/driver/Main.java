ccf559ed42d31755df0b5415772f66ead5c4e9cc
Mukul <mukul.devry99@gmail.com>
2024-02-26 18:31:28 +0530

    new

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,26 @@
 package com.driver;
 
 public class Main {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         B obj = new B();
-      String ans = obj.meth();
-      System.out.println(ans);
-//    System.out.println(obj.meth());
+//      String ans = obj.meth();
+//      System.out.println(ans);
+        System.out.println(obj.meth());
+    }
+
+    public static class A {
+        public String meth() {
+//        String str = "Invoking method from class A";
+//        return str;
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+        public String meth() {
+//        String str = "Invoking method from class A";
+//        return str;
+            return "Method is overridden in Extendend class B";
+        }
     }
 }
